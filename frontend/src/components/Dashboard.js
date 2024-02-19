05fc728894a9183b8971a3b067014bb9bd67507e
dt0wns <dennis@dennistowns.com>
2024-02-19 13:55:15 -0500

    dashboard get accounts for logged in user

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axiosInstance from "../axios";
+
 
 const Dashboard = () => {
+    const [accounts , setAccounts] = useState([])
+
+    useEffect(() => {
+        axiosInstance.get('bank/accounts/')
+            .then((res) => {
+                setAccounts(res.data)
+                console.log(res.data)
+            });
+    }, []);
+
+
     return (
         <div>
         <h1>Dashboard</h1>
+        {accounts.map((account) => (
+            <div key={account.id}>
+                <h2>{account.account_number}</h2>
+                <p>{account.account_id}</p>
+                <p>{account.balance}</p>
+            </div>
+        ))}
         </div>
     );
     }
