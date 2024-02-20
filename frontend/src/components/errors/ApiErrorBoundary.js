b5aae49d329a304f07b8333c5ee5d738bc6a560f
dt0wns <dennis@dennistowns.com>
2024-02-20 13:20:30 -0500

    cleanup

--- frontend/src/components/errors/ApiErrorBoundary.js
+++ frontend/src/components/errors/ApiErrorBoundary.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const ApiErrorBoundary = (props) => {
-    const { error, errorInfo } = props;
+    const { error } = props;
     return (
         error && Object.keys(error).map((key) => (
             <p key={key} style={{color: 'red'}}>{key}: {error[key]}</p>
